c133b724de1ec67c409d2514532e885056f1b634
Pro1d <you@example.com>
2024-08-18 21:56:20 +0200

    fix: fish speed

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -77,7 +77,7 @@ func _physics_process(delta: float) -> void:
 	# Move
 	var lon_vel := command_direction.dot(velocity)
 	if lon_vel < max_speed:
-		lon_vel = lon_vel + maxf(accel * delta, max_speed - lon_vel)
+		lon_vel = lon_vel + minf(accel * delta, max_speed - lon_vel)
 	velocity += command_direction * lon_vel - command_direction * command_direction.dot(velocity)
 	var d := FROZEN_DRAG if state == State.FROZEN else drag
 	velocity = velocity * pow(1.0 - d, delta)
