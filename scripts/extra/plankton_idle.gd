481b69ca97be3f4c500cdf125dba8cd8ef685aa6
Frank Bessou <frank.bessou@logilab.fr>
2024-08-18 13:13:44 +0200

    feat: enhance plankton animation on opening

--- scripts/extra/plankton_idle.gd
+++ scripts/extra/plankton_idle.gd
@@ -8,6 +8,7 @@ var moving : bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	play("idle")
+	set_frame_and_progress(randi()%2, randf())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
