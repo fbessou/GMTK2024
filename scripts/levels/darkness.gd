287efed741ce1f6438c0da6c3fbf3284ca035717
Pro1d <you@example.com>
2024-08-20 00:39:33 +0200

    anim camera wip

--- scripts/levels/darkness.gd
+++ scripts/levels/darkness.gd
@@ -1,9 +1,25 @@
 extends Level
 
+
+@onready var animation := $AnimationPlayer as AnimationPlayer
+
 func _ready() -> void:
 	super()
 	ProjectSettings.set_setting("rendering/environment/defaults/default_clear_color", Color.BLACK)
-	#for c in $Environment/Cave.get_children():
-		#var lo := c.find_child("LightOccluder2D") as LightOccluder2D
-		#if lo == null: continue
-		#lo.occluder.cull_mode = OccluderPolygon2D.CULL_CLOCKWISE
+	var area := $WaterCurrents/WaterCurrent8 as Area2D
+	area.body_entered.connect(_on_body_entered_tunnel)
+
+func _on_body_entered_tunnel(body: Node2D) -> void:
+	var fish := body as Fish
+	if fish == null:
+		return
+	if fish.state != Fish.State.PLAYER:
+		return
+	
+	# Play ending animation with Cthulhu
+	fish.freeze()
+	($Angler/Camera2D as Camera2D).enabled = false
+	($AnimationPlayer/Camera2D as Camera2D).enabled = true
+	animation.play("ending")
+	await animation.animation_finished
+	print("THE END")
