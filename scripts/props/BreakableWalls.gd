55fac2923093851f661d37bc0737a3d3a33f5a5d
ValentinWB <valentinwantzbessou@gmail.com>
2024-08-19 13:15:14 +0200

    Breakable wall first implementation

---
+++ scripts/props/BreakableWalls.gd
@@ -0,0 +1,17 @@
+class_name BreakableWall
+extends Node
+
+@export var speedThreshold: float = 3000.
+@onready var collision_shape_2d: CollisionShape2D = $Colliders/RigidBody2D/CollisionShape2D
+
+var isBroken: bool = false
+
+func _on_body_entered(body: Node2D) -> void:
+	var fish := body as Fish
+	if(fish == null):
+		return
+	
+	if(fish.velocity.length() >= speedThreshold):
+		isBroken = false
+		collision_shape_2d.hide()
+		collision_shape_2d.queue_free()
