601690559cae8adbedde054a8b9224336ae2eeb5
Pro1d <you@example.com>
2024-08-20 00:17:46 +0200

    darkness level wip

--- scripts/levels/darkness.gd
+++ scripts/levels/darkness.gd
@@ -2,6 +2,7 @@ extends Level
 
 func _ready() -> void:
 	super()
+	ProjectSettings.set_setting("rendering/environment/defaults/default_clear_color", Color.BLACK)
 	#for c in $Environment/Cave.get_children():
 		#var lo := c.find_child("LightOccluder2D") as LightOccluder2D
 		#if lo == null: continue
